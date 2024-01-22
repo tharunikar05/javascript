8bfea856fab80ff6a4a811608c1e9c6d804fe040
unknown <tharunikar.21ece@kongu.edu>
2024-01-21 19:38:02 -0800

    Alpha

---
+++ Javascrip_operators.js
@@ -0,0 +1,33 @@
+//Assignament operator
+var a=10
+console.log("Assignment operator output",a)
+//Arithmetic operator
+var aA=10
+var bA=10
+console.log(typeof(aA))
+console.log("Arithmetic operator addition output",aA+bA)
+console.log("Arithmetic operator  subraction output",aA-bA)
+console.log("Arithmetic operator multiplication output",aA*bA)
+console.log("Arithmetic operator division output",aA/bA)
+console.log("Arithmetic operator modulus output",aA%bA)
+console.log("Arithmetic operator power output",aA**bA)
+console.log("Arithmetic operator Increment output",aA++)
+console.log("Arithmetic operator Decrement output",bA--)
+console.log("Arithmetic operator Increment output",++aA)
+console.log("Arithmetic operator Increment output",--aA)
+
+//logical operator-> &&,||,!
+var aL=20
+var bL=10
+console.log("logical operator AND output",aL&&bL)
+console.log("logical operator || output",aL||bL)
+console.log("logical operator ! output",!aL)
+console.log(10&&20)
+console.log(10&&30)
+console.log(10||20)
+console.log(0||20)
+console.log(0||20)
+console.log(100||20&&0)//In or condition if first condition true it executed not consider second condition
+console.log((100||20)&&0)//In or condition if first condition false it executed second conditionconsole.log((100||20)&&0)
+console.log(0&&(100||20))
+console.log(0&&100||20)
