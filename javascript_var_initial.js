786650562193a7629294e2937d04063da6851ea7
unknown <tharunikar.21ece@kongu.edu>
2024-01-21 19:34:45 -0800

    javascript

---
+++ javascript_var_initial.js
@@ -0,0 +1,10 @@
+a='a'
+console.log("General declaration of variable",a);
+var b="var";
+console.log("This variable belongs to var type value is",b)
+var b=10;
+console.log("This variable belongs to changed var type value is",b)
+let c='let';
+console.log("This variable belongs to let type value is",c)
+const d=200;
+console.log("This variable belongs to const type value is",d)
